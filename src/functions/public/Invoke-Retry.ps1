3db965fc64fbca7917edfa218fd3865a05b98abd
Marius Storhaug <marstor@hotmail.com>
2024-10-08 14:56:38 +0200

    Initial release of Retry (#1)
    * Initial release of Retry
    
    * Fix
    
    * Disable prettier

---
+++ src/functions/public/Invoke-Retry.ps1
@@ -0,0 +1,46 @@
+﻿function Invoke-Retry {
+    <#
+        .SYNOPSIS
+        Runs a scriptblock with a retry mechanism.
+
+        .EXAMPLE
+        Retry -Count 5 -Delay 5 -Run {
+            Invoke-RestMethod -Uri 'https://api.myip.com/'
+        }
+    #>
+    [CmdletBinding()]
+    [Alias('Retry')]
+    param(
+        # The number of tries to make
+        [int] $Count = 3,
+
+        # The delay between tries in seconds
+        [int]$Delay = 5,
+
+        # The scriptblock to run
+        [scriptblock] $Run,
+
+        # A scriptblock to run if it fails
+        [scriptblock]$Catch = {}
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    for ($i = 0; $i -lt $Count; $i++) {
+        try {
+            & $Run
+            break
+        } catch {
+            Write-Warning "The command:"
+            Write-Warning $Run.ToString()
+            Write-Warning "failed with error: $_"
+            & $Catch
+            if ($i -eq $Count - 1) {
+                throw
+            }
+
+            Write-Warning "Retrying in $Delay seconds..."
+            Start-Sleep -Seconds $Delay
+        }
+    }
+}
---
+++ tests/Retry.Tests.ps1
@@ -0,0 +1,23 @@
+﻿[CmdletBinding()]
+Param(
+    # Path to the module to test.
+    [Parameter()]
+    [string] $Path
+)
+
+Write-Verbose "Path to the module: [$Path]" -Verbose
+
+Describe 'Module' {
+    Context 'Function: Get-PSModuleTest' {
+        It "Invoke-Retry -Count 5 -Delay 5 {...}" {
+            { Invoke-Retry -Count 5 -Delay 5 {
+                Invoke-RestMethod -Uri 'hhttps://api.myip.com/'
+            } | Should -Not -Throw }
+        }
+        It "Retry 5 5 {...}" {
+            { Retry 5 5 {
+                Invoke-RestMethod -Uri 'hhttps://api.myip.com/'
+            } | Should -Not -Throw }
+        }
+    }
+}
