bb99ec2e4f9589360982e7e9a795824feaa47f01
Marius Storhaug <marstor@hotmail.com>
2024-10-09 12:03:37 +0200

    🚀 [Feature] Change param order and add `-Finally` (#4)
    ## Description
    
    - Add description
    - Reorganize so Run comes first
    - Allow Invoke-Retry to run with finally
    - Add new tests for changed param order
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [x] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/Retry.Tests.ps1
+++ tests/Retry.Tests.ps1
@@ -7,17 +7,28 @@ Param(
 
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
-Describe 'Module' {
-    Context 'Function: Get-PSModuleTest' {
-        It "Invoke-Retry -Count 5 -Delay 5 {...}" {
-            { Invoke-Retry -Count 5 -Delay 5 {
-                Invoke-RestMethod -Uri 'hhttps://api.myip.com/'
-            } | Should -Not -Throw }
+Describe 'Functions' {
+    Context 'Invoke-Retry' {
+        It 'Invoke-Retry -Count 5 -Delay 5 {...} should not throw' {
+            {
+                Invoke-Retry -Count 5 -Delay 5 {
+                    Invoke-RestMethod -Uri 'https://api.myip.com/'
+                }
+            } | Should -Not -Throw
         }
-        It "Retry 5 5 {...}" {
-            { Retry 5 5 {
-                Invoke-RestMethod -Uri 'hhttps://api.myip.com/'
-            } | Should -Not -Throw }
+        It 'Retry {...} should not throw' {
+            {
+                Retry {
+                    Invoke-RestMethod -Uri 'https://api.myip.com/'
+                }
+            } | Should -Not -Throw
+        }
+        It 'Retry -Count 5 -Delay 5 {...} should not throw' {
+            {
+                Retry -Count 5 -Delay 5 {
+                    Invoke-RestMethod -Uri 'https://api.myip.com/'
+                }
+            } | Should -Not -Throw
         }
     }
 }
