784eb6f7b4941c6ebb53db3050188efe97044e7c
Marius Storhaug <marstor@hotmail.com>
2024-10-09 11:47:39 +0200

    Add tests

--- tests/Retry.Tests.ps1
+++ tests/Retry.Tests.ps1
@@ -7,17 +7,28 @@ Param(
 
 Write-Verbose "Path to the module: [$Path]" -Verbose
 
-Describe 'Module' {
-    Context 'Function: Get-PSModuleTest' {
-        It "Invoke-Retry -Count 5 -Delay 5 {...}" {
-            { Invoke-Retry -Count 5 -Delay 5 {
-                Invoke-RestMethod -Uri 'hhttps://api.myip.com/'
-            } | Should -Not -Throw }
+Describe 'Functions' {
+    Context 'Invoke-Retry' {
+        It 'Invoke-Retry -Count 5 -Delay 5 {...}' {
+            {
+                Invoke-Retry -Count 5 -Delay 5 {
+                    Invoke-RestMethod -Uri 'hhttps://api.myip.com/'
+                }
+            } | Should -Not -Throw
         }
-        It "Retry 5 5 {...}" {
-            { Retry 5 5 {
-                Invoke-RestMethod -Uri 'hhttps://api.myip.com/'
-            } | Should -Not -Throw }
+        It 'Retry {...}' {
+            {
+                Retry {
+                    Invoke-RestMethod -Uri 'hhttps://api.myip.com/'
+                }
+            } | Should -Not -Throw
+        }
+        It 'Retry -Count 5 -Delay 5 {...}' {
+            {
+                Retry -Count 5 -Delay 5 {
+                    Invoke-RestMethod -Uri 'hhttps://api.myip.com/'
+                }
+            } | Should -Not -Throw
         }
     }
 }
