44dfc928b972b0356cab0d88d6faa97d407c6cff
Marius Storhaug <marstor@hotmail.com>
2024-11-02 17:49:01 +0100

    🩹 [Patch]: Remove redundant parameter from tests (#5)
    ## Description
    
    - Remove redundant parameter from tests
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/Retry.Tests.ps1
+++ tests/Retry.Tests.ps1
@@ -1,13 +1,4 @@
-﻿[CmdletBinding()]
-Param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'Functions' {
+﻿Describe 'Functions' {
     Context 'Invoke-Retry' {
         It 'Invoke-Retry -Count 5 -Delay 5 {...} should not throw' {
             {
